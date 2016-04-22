f6eb9d41b578319917d71fc9d635dac900dafc40
Zander Baldwin <hello@zanderbaldwin.com>
2016-04-22 13:34:06 +0100

    Add Stack Middlewares

--- web/app.php
+++ web/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use Darsyn\Stack\RequestId\Injector as RequestIdInjector;
+use Darsyn\Stack\RequestId\UuidGenerator;
+use Stack\Builder as StackBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\TerminableInterface;
 
@@ -9,23 +12,24 @@ use Symfony\Component\HttpKernel\TerminableInterface;
 $loader = require __DIR__ . '/../app/autoload.php';
 include_once __DIR__ . '/../var/bootstrap.php.cache';
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+// Enable APC for autoloading to improve performance. You should change the ApcClassLoader first argument to a unique
+// prefix in order to prevent cache key conflicts with other applications also using APC.
+# $apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
+# $loader->unregister();
+# $apcLoader->register(true);
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+# $kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+$stack = (new StackBuilder)
+    ->push(RequestIdInjector::class, new UuidGenerator(getenv('APPLICATION_NODE') ?: null))
+    ->resolve($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the
+// configuration parameter
+# Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);
+$response = $stack->handle($request);
 $response->send();
 $kernel instanceof TerminableInterface && $kernel->terminate($request, $response);
--- web/dev.php
+++ web/dev.php
@@ -1,23 +1,16 @@
 <?php
 
+use Darsyn\Stack\IpRestrict\Whitelist as IpWhitelist;
+use Darsyn\Stack\RequestId\Injector as RequestIdInjector;
+use Darsyn\Stack\RequestId\UuidGenerator;
+use Stack\Builder as StackBuilder;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\TerminableInterface;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line. For more information,
 // read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-}
+# umask(0000);
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
@@ -27,7 +20,13 @@ Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+
+$stack = (new StackBuilder)
+    ->push(IpWhitelist::class, ['127.0.0.1', 'fe80::1', '::1', '192.168.0.0/16'])
+    ->push(RequestIdInjector::class, new UuidGenerator(getenv('APPLICATION_NODE') ?: null))
+    ->resolve($kernel);
+
 $request = Request::createFromGlobals();
-$response = $kernel->handle($request);
+$response = $stack->handle($request);
 $response->send();
 $kernel instanceof TerminableInterface && $kernel->terminate($request, $response);
